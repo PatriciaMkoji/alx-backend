55eb14c82b16087db30fe323a22be4485ac345a7
PatriciaMkoji <keyshotrisha@gmail.com>
2024-01-31 12:24:38 +0300

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python4
 """
 Basic Caching Dictionary
 """
 from base_caching import BaseCaching
 
 
-class BasicCache():
+class BasicCache(BaseCaching):
     """ A basic caching system
     - You must use self.cache_data - dictionary from parent class BaseCaching
     - This caching system doesn’t have limit
@@ -16,6 +16,10 @@ class BasicCache():
         ~ Must return the value in self.cache_data linked to key
         ~ If key is None / doesn’t exist in self.cache_data, return None
     """
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+
     def put(self, key, item):
         """ Add an item to the cache """
         if key is not None and item is not None:
@@ -23,8 +27,6 @@ class BasicCache():
 
     def get(self, key):
         """ Get an item from cache by key """
-        self.cache_data = key
-
         if key is not None:
             return self.cache_data.get(key, None)
         else:
